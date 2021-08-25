1814494f9c57c4b6926d490e7290b237ec6e7152
Natanael <natanael.spindola@getinfo.tech>
2021-08-24 22:19:49 -0300

    Segundo Commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace repositorio
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! Teste");
         }
     }
 }
